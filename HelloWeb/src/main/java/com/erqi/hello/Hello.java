be6efcdedb57ad5f4bd0697bbee868fa7cb111df
ErQi <40307204@qq.com>
2017-03-19 00:23:29 +0800

    web项目添加依赖有点奇怪... 并不是在项目HelloWeb下添加,反而是在项目HelloWeb_main下添加依赖,同样的还有一个项目HelloWeb_test子项目.... 好坑爹...添加了半天的依赖没效果...

--- HelloWeb/src/main/java/com/erqi/hello/Hello.java
+++ HelloWeb/src/main/java/com/erqi/hello/Hello.java
@@ -1,9 +1,11 @@
 package com.erqi.hello;
 
+import javax.servlet.http.HttpServlet;
+
 /**
  * 作 者: ErQi
  * 时 间: 2017-3-18
- * 备 注: TODO
+ * 备 注: 无
  */
-public class Hello {
+public class Hello extends HttpServlet{
 }
