292c5e1ceb24df424b791a8a2b22a3ef6939a0c8
ErQi <40307204@qq.com>
2017-03-19 01:11:26 +0800

    3.0默认去掉了web.xml,现在通过@Webxxxx注解的方式配置相关信息

--- HelloWeb/src/main/java/com/erqi/hello/Hello.java
+++ HelloWeb/src/main/java/com/erqi/hello/Hello.java
@@ -1,11 +1,38 @@
 package com.erqi.hello;
 
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 作 者: ErQi
  * 时 间: 2017-3-18
  * 备 注: 无
  */
-public class Hello extends HttpServlet{
+@WebServlet( urlPatterns = { "/hello"})
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.println("  <h1>");
+        out.print("    This is ");
+        out.print(this.getClass());
+        out.println(", using the GET method");
+        out.println("</h1>");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+    }
 }
