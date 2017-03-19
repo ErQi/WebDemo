208567a5eecaaad903edb187cadcd32752c17c71
ErQi <40307204@qq.com>
2017-03-19 18:40:22 +0800

    测试了半天,重装了两次,暂时确定idea并不支持gradle自动通过dependencies搜索而自动依赖. 去社区提问,别人回复要在build.gradle里添加,要我看看gradle基础  orz...才知道idea并不是自动添加

--- HelloWeb/src/main/java/com/erqi/hello/Hello.java
+++ HelloWeb/src/main/java/com/erqi/hello/Hello.java
@@ -1,6 +1,5 @@
 package com.erqi.hello;
 
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,16 +14,30 @@ import java.sql.SQLException;
  * 时 间: 2017-3-18
  * 备 注: 无
  */
-@WebServlet( urlPatterns = { "/hello"})
+@WebServlet(urlPatterns = {"/hello"})
 public class Hello extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         UserService service = new UserService();
+        User login = null;
         try {
-            service.login("123","456");
+            login = service.login("tom", "123");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.println("  <h1>");
+        out.print("id:" + (login == null ? "" : login.getId()));
+        out.println("</h1>");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
     }
 }
--- HelloWeb/src/main/java/com/erqi/hello/UserDao.java
+++ HelloWeb/src/main/java/com/erqi/hello/UserDao.java
@@ -1,41 +1,28 @@
 package com.erqi.hello;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDao {
 
-	/**
-	 * 登录
-	 * @param username 用户名
-	 * @param password 密码
-	 * @return 用户
-	 * @throws SQLException 
-	 */
-	public User getUserByUsernameAndPwd(String username, String password) throws SQLException {
-		//创建queryrunner
-//		QueryRunner qr = new QueryRunner(DataSourceUtils.getDataSource());
-		onTest();
-		//编写sql
-		System.setProperty("com.mchange.v2.c3p0.cfg.xml","classloader:c3p0-config.xml");
-		String sql="select * from user where username = tom and password = 123";
-		ComboPooledDataSource source = new ComboPooledDataSource();
-		System.err.println(source.getUser());
-		System.err.println(source.getPassword());
-		Connection connection = source.getConnection();
-		CallableStatement call = connection.prepareCall(sql);
-		ResultSet set = call.executeQuery();
-		//执行sql
-//		User user = qr.query(sql, new BeanHandler<User>(User.class), username,password);
-		return new User();
-	}
-
-	private void onTest() {
-		ClassLoader classLoader = this.getClass().getClassLoader();
-	}
-
+    /**
+     * 登录
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return 用户
+     * @throws SQLException
+     */
+    public User getUserByUsernameAndPwd(String username, String password) throws SQLException {
+        //创建queryrunner
+        QueryRunner qr = new QueryRunner(new ComboPooledDataSource());
+        //编写sql
+        String sql = "select * from user where username = ? and password = ?";
+        //执行sql
+        User user = qr.query(sql, new BeanHandler<User>(User.class), username, password);
+        return new User();
+    }
 }
