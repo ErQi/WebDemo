8264e41d569155a878b57406caf117bf708db43b
ErQi <40307204@qq.com>
2017-04-17 19:29:32 +0800

    struts2拦截器能正确的修改get和post的参数编码,因此注释掉编码的filter

--- HibernateDemo/src/main/java/com/erqi/domain/User.java
+++ HibernateDemo/src/main/java/com/erqi/domain/User.java
@@ -42,4 +42,13 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "account='" + account + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
